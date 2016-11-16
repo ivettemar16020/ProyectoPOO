5476da38272fb6aad79c12823ae410038c2a0730
rec16005 <rec16005@uvg.edu.gt>
2016-11-16 17:05:06 -0600

    Ya se pueden crear usuarios :D

--- CrearUsr.java
+++ CrearUsr.java
@@ -4,19 +4,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import BaseDatos.MySQL;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
 public class CrearUsr extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txt_User;
-	private JPasswordField passwordField;
+	private JPasswordField pss_Pss;
 
 	/**
 	 * Launch the application.
@@ -67,11 +75,56 @@ public class CrearUsr extends JFrame {
 		panel.add(txt_User);
 		txt_User.setColumns(10);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(213, 103, 86, 20);
-		panel.add(passwordField);
+		pss_Pss = new JPasswordField();
+		pss_Pss.setBounds(213, 103, 86, 20);
+		panel.add(pss_Pss);
+		
+		MySQL mysql = new MySQL();
+		Connection cn = mysql.Conectar();
 		
 		JButton btnCrear = new JButton("Crear");
+		btnCrear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btnCrear){
+					
+					try {
+						String accion = "insertar";
+						MySQL mysql = new MySQL();
+						Connection cn = mysql.Conectar();
+						
+						String user = txt_User.getText();
+						String pass = pss_Pss.getText();
+						
+						
+						//Inserta los datos en la base de datos
+						String sSQL = "INSERT INTO usuario(nombreUsuario, contrasena)"
+								+ "VALUES(?,?)";
+						String mensaje = "Los datos se han insertado de manera satisfactoria";
+						
+						PreparedStatement pst = cn.prepareStatement(sSQL);
+						pst.setString(1, user);
+						pst.setString(2, pass);
+						
+						
+						int n = pst.executeUpdate();
+						
+						//Vacía los textfields
+						txt_User.setText("");
+						pss_Pss.setText("");
+						
+						
+						if (n < 0){
+							JOptionPane.showMessageDialog(null, mensaje);
+						}
+						
+						
+					} catch (SQLException e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
+				}
+			}
+		});
 		btnCrear.setBounds(85, 158, 89, 23);
 		panel.add(btnCrear);
 		
